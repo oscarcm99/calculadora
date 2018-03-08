504143a6808b5f9a78e0d9697c153ca596a71982
oscarcm99 <gorri0302@gmail.com>
2018-03-08 22:05:40 +0100

    Acabadas pruebas de regresion del codigo ya escrito de la suma,resta,mul y div

--- calculadora2.0/CalculadoraTest.java
+++ calculadora2.0/CalculadoraTest.java
@@ -45,159 +45,201 @@ public class CalculadoraTest
     }
     @Test
     public void suma(){
-        //Caso 1 suma
+        //Caso 1 suma probamos con dos numeros reales positivos
         calculadora.ponNum1(1);
         calculadora.ponNum2(4);
         calculadora.ponOperacion("SUMA");
         calculadora.opera();
-        assertEquals(5, calculadora.resultadoReal(),0.1);
-        //Caso 2 suma
+        assertEquals(5, calculadora.resultadoReal(),0.1);//0,1 es el valor de desborde de un double
+        //Caso 2 suma con dos numeros reales negativos
         calculadora.ponNum1(-1);
         calculadora.ponNum2(-2);
         calculadora.ponOperacion("SUMA");
         calculadora.opera();
         assertEquals(-3, calculadora.resultadoReal(),0.1);
-        //Caso 3 suma
+        //Caso 3 suma probamos el 0 como segundo operando
         calculadora.ponNum1(5.5);
         calculadora.ponNum2(0);
         calculadora.ponOperacion("SUMA");
         calculadora.opera();
         assertEquals(5.5, calculadora.resultadoReal(),0.1);
-        //Caso 4 suma
+        //Caso 4 suma probamos con el 0 la propiedad conmutativa si se cumple
         calculadora.ponNum1(0);
-        calculadora.ponNum2(1.3);
+        calculadora.ponNum2(5.5);
         calculadora.ponOperacion("SUMA");
         calculadora.opera();
-        assertEquals(1.3, calculadora.resultadoReal(),0.1);
-        //Caso 5 suma
+        assertEquals(5.5, calculadora.resultadoReal(),0.1);
+        //Caso 5 suma ponemos los valores limite positivos del double y da infinito como resultado
         calculadora.ponNum1(Double.MAX_VALUE);
         calculadora.ponNum2(Double.MAX_VALUE);
         calculadora.ponOperacion("SUMA");
         calculadora.opera();
         assertEquals(Double.POSITIVE_INFINITY, calculadora.resultadoReal(),0.1);
-        //Caso 6 suma
+        //Caso 6 suma ponemos los valores limite negativos del double y da menos infinito como resultado
         calculadora.ponNum1(-Double.MAX_VALUE);
         calculadora.ponNum2(-Double.MAX_VALUE);
         calculadora.ponOperacion("SUMA");
         calculadora.opera();
         assertEquals(Double.NEGATIVE_INFINITY, calculadora.resultadoReal(),0.1);
+        //Caso 7 ponemos los valores limite positivos y negativos y espera un 0
+        calculadora.ponNum1(Double.MAX_VALUE);
+        calculadora.ponNum2(-Double.MAX_VALUE);
+        calculadora.ponOperacion("SUMA");
+        calculadora.opera();
+        assertEquals(0, calculadora.resultadoReal(),0.1);
     }
     @Test
     public void resta(){
-        //Caso 1 resta
+        //Caso 1 resta ponemos dos numeros reales y el resultado sale negativo
         calculadora.ponNum1(2.2);
         calculadora.ponNum2(4);
         calculadora.ponOperacion("RESTA");
         calculadora.opera();
         assertEquals(-1.8, calculadora.resultadoReal(),0.1);
-        //Caso 2 resta
+        //Caso 2 resta probamos que al cambiar los operandos el resultado es distinto (positivo)
         calculadora.ponNum1(4);
         calculadora.ponNum2(2.2);
         calculadora.ponOperacion("RESTA");
         calculadora.opera();
         assertEquals(1.8, calculadora.resultadoReal(),0.1);
-        //Caso 3 resta
+        //Caso 3 resta probamos el 0 que no modifica el valor del primer numero
         calculadora.ponNum1(4);
         calculadora.ponNum2(0);
         calculadora.ponOperacion("RESTA");
         calculadora.opera();
         assertEquals(4, calculadora.resultadoReal(),0.1);
-        //Caso 4 resta
+        //Caso 4 resta probamos que al cambiar el 0 por el 4 cambia el resultado
         calculadora.ponNum1(0);
         calculadora.ponNum2(4);
         calculadora.ponOperacion("RESTA");
         calculadora.opera();
         assertEquals(-4, calculadora.resultadoReal(),0.1);
-        //Caso 5 resta
+        //Caso 5 resta al poner el valor limite positivo menos el negativo da infinto
         calculadora.ponNum1(Double.MAX_VALUE);
         calculadora.ponNum2(-Double.MAX_VALUE);
         calculadora.ponOperacion("RESTA");
         calculadora.opera();
         assertEquals(Double.POSITIVE_INFINITY, calculadora.resultadoReal(),0.1);
-       //Caso 6 resta
+       //Caso 6 resta poner valor limite  negativo menos el positivo es igual a menos infinito
         calculadora.ponNum1(-Double.MAX_VALUE);
         calculadora.ponNum2(Double.MAX_VALUE);
         calculadora.ponOperacion("RESTA");
         calculadora.opera();
         assertEquals(Double.NEGATIVE_INFINITY, calculadora.resultadoReal(),0.1);
+        //Caso 7 resta poner los valores limites negativos espera un 0 porque - -(-) = -+
+        calculadora.ponNum1(-Double.MAX_VALUE);
+        calculadora.ponNum2(-Double.MAX_VALUE);
+        calculadora.ponOperacion("RESTA");
+        calculadora.opera();
+        assertEquals(0, calculadora.resultadoReal(),0.1);
     }
     @Test
     public void multiplicacion(){
-        //Caso 1 multiplicacion
+        //Caso 1 multiplicacion dos numeros reales
         calculadora.ponNum1(3.1);
         calculadora.ponNum2(5);
         calculadora.ponOperacion("MULTIPLICA");
         calculadora.opera();
         assertEquals(15.5, calculadora.resultadoReal(),0.1);
-        //Caso 2 multiplicacion
+        //Caso 2 multiplicacion dos numeros reales invertidos y no cambia el resultado
         calculadora.ponNum1(5);
         calculadora.ponNum2(3.1);
         calculadora.ponOperacion("MULTIPLICA");
         calculadora.opera();
         assertEquals(15.5, calculadora.resultadoReal(),0.1);
-        //Caso 3 multiplicacion
+        //Caso 3 multiplicacion por 0
         calculadora.ponNum1(0);
         calculadora.ponNum2(5);
         calculadora.ponOperacion("MULTIPLICA");
         calculadora.opera();
         assertEquals(0, calculadora.resultadoReal(),0.1);
-        //Caso 4 multiplicacion
+        //Caso 4 multiplicacion por 0 invirtiendo valores y no cambia
         calculadora.ponNum1(5);
         calculadora.ponNum2(0);
         calculadora.ponOperacion("MULTIPLICA");
         calculadora.opera();
         assertEquals(0, calculadora.resultadoReal(),0.1);
-        //Caso 5 multiplicacion
+        //Caso 5 multiplicacion limite positivo por limite positivo espera infinito
         calculadora.ponNum1(Double.MAX_VALUE);
         calculadora.ponNum2(Double.MAX_VALUE);
         calculadora.ponOperacion("MULTIPLICA");
         calculadora.opera();
         assertEquals(Double.POSITIVE_INFINITY, calculadora.resultadoReal(),0.1);
-        //Caso 6 multiplicacion
+        //Caso 6 multiplicacion limite negativo por limite positivo espera menos infinito
         calculadora.ponNum1(-Double.MAX_VALUE);
         calculadora.ponNum2(Double.MAX_VALUE);
         calculadora.ponOperacion("MULTIPLICA");
         calculadora.opera();
         assertEquals(Double.NEGATIVE_INFINITY, calculadora.resultadoReal(),0.1);
+        //Caso 7 multiplicacion limite negativo por limite negativo espera
+        calculadora.ponNum1(-Double.MAX_VALUE);
+        calculadora.ponNum2(-Double.MAX_VALUE);
+        calculadora.ponOperacion("MULTIPLICA");
+        calculadora.opera();
+        assertEquals(Double.POSITIVE_INFINITY, calculadora.resultadoReal(),0.1);
     }
     @Test
     public void division(){
-        //Caso 1 division
+        //Caso 1 division entre dos reales
         calculadora.ponNum1(4);
         calculadora.ponNum2(2);
         calculadora.ponOperacion("DIVIDE");
         calculadora.opera();
         assertEquals(2, calculadora.resultadoReal(),0.1);
-        //Caso 2 division
+        //Caso 2 division dos reales invertidos
         calculadora.ponNum1(2);
         calculadora.ponNum2(4);
         calculadora.ponOperacion("DIVIDE");
         calculadora.opera();
         assertEquals(0.5, calculadora.resultadoReal(),0.1);
-        //Caso 3 division
+        //Caso 3 division 0 entre un divisor distinto a 0 espera 5
         calculadora.ponNum1(0);
         calculadora.ponNum2(5);
         calculadora.ponOperacion("DIVIDE");
         calculadora.opera();
         assertEquals(0, calculadora.resultadoReal(),0.1);
-        //Caso 4 division
+        //Caso 4 division numero real entre 0 espera infinito
         calculadora.ponNum1(5);
         calculadora.ponNum2(0);
         calculadora.ponOperacion("DIVIDE");
         calculadora.opera();
         assertEquals(Double.POSITIVE_INFINITY, calculadora.resultadoReal(),0.1);
-       //Caso 5 division
+       //Caso 5 division espera un 0
         calculadora.ponNum1(0);
         calculadora.ponNum2(Double.MAX_VALUE);
         calculadora.ponOperacion("DIVIDE");
         calculadora.opera();
         assertEquals(0, calculadora.resultadoReal(),0.1);
-       //Caso 6 division
+       //Caso 6 division espera uno ya que se dividen dos valores iguales
         calculadora.ponNum1(Double.MAX_VALUE);
         calculadora.ponNum2(Double.MAX_VALUE);
         calculadora.ponOperacion("DIVIDE");
         calculadora.opera();
         assertEquals(1.0, calculadora.resultadoReal(),0.1);
+        //Caso 7 division se divide 0 entre 0 y no sale numero
+        calculadora.ponNum1(0);
+        calculadora.ponNum2(0);
+        calculadora.ponOperacion("DIVIDE");
+        calculadora.opera();
+        assertEquals(Double.NaN, calculadora.resultadoReal(),0.1);
+        //Caso 8 division negativo entre 0
+        calculadora.ponNum1(-5);
+        calculadora.ponNum2(0);
+        calculadora.ponOperacion("DIVIDE");
+        calculadora.opera();
+        assertEquals(Double.NEGATIVE_INFINITY, calculadora.resultadoReal(),0.1);
+        // Caso 9 division limite positivo entre limite negativo
+        calculadora.ponNum1(Double.MAX_VALUE);
+        calculadora.ponNum2(-Double.MAX_VALUE);
+        calculadora.ponOperacion("DIVIDE");
+        calculadora.opera();
+        assertEquals(-1.0, calculadora.resultadoReal(),0.1);
+        //Caso 10 division limite negativo entre limite negativo
+        calculadora.ponNum1(-Double.MAX_VALUE);
+        calculadora.ponNum2(-Double.MAX_VALUE);
+        calculadora.ponOperacion("DIVIDE");
+        calculadora.opera();
+        assertEquals(1, calculadora.resultadoReal(),0.1);
     }
     @Test
     public void raices(){
